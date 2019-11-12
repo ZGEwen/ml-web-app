bd045fc69efa348448dd50c770d26afd7a962e88
zgw <2545717263@qq.com>
2019-11-12 21:15:01 +0800

    添加绘画示例

---
+++ plotting.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python3.7
+# coding:utf-8
+"""
+@author: zgw
+@file: plotting.py
+@date: 2019/11/12 14:21
+@description 绘图演示
+"""
+import streamlit as st
+import time
+import numpy as np
+
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
